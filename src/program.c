dc082d402700cf7f9926ebde40917294fcba09f1
Enrico Rossi <e.rossi@tecnobrain.com>
2011-07-19 20:31:38 +0200

    fix: now allarms work ok

--- src/program.c
+++ src/program.c
@@ -177,7 +177,7 @@ uint8_t prog_del(struct programs_t *progs, const uint8_t n)
 uint8_t prog_allarm(struct programs_t *progs)
 {
 	/* if there is an allarm */
-	if (!io_in_allarm()) {
+	if (io_in_allarm()) {
 		io_out_change_line(0xff, 0); /* close all the lines */
 		progs->qc = 0; /* remove all progs in the queue */
 		return(1);
